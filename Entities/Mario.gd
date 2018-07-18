b4cba4a2f4830041b2ec9b772335381934aa8a55
Ilos <oliver-lehmann@freenet.de>
2018-07-18 13:11:30 +0200

    update level and mario control

--- Entities/Mario.gd
+++ Entities/Mario.gd
@@ -6,8 +6,9 @@ signal hit_by_enemy
 #World_Constants
 const GRAVITY = 2000.0
 const JUMPFORCE = 1000.0
-const WALKSPEED = 25.0
-const MAX_SPEED = 600.0
+const WALKFORCE = 25.0
+const MAX_SPEED_WALK = 600.0
+const MAX_SPEED_SPRINT = 900.0
 const STOP_FORCE = 4000.0
 
 #movement-Vector
@@ -21,6 +22,7 @@ var state
 
 #bool to play Animation
 var animation_playing = false
+var anim = ""
 
 #---------------------------------------------------------
 func _ready():
@@ -36,15 +38,19 @@ func _physics_process(delta):
 
 	# set behavior for pressing left or right
 	if Input.is_action_pressed("ui_right"):
-		movement.x += WALKSPEED
+		movement.x += WALKFORCE
 		$Sprite_Small.flip_h = false
 		state = WALK if state!=JUMP else JUMP
-		if state == WALK and movement.x < 0: state = SLIDE
+		if state == WALK and movement.x < 0:
+			state = SLIDE
+
 	elif Input.is_action_pressed("ui_left"):
-		movement.x -= WALKSPEED
+		movement.x -= WALKFORCE
 		$Sprite_Small.flip_h = true
 		state = WALK if state!=JUMP else JUMP
-		if state == WALK and movement.x > 0: state = SLIDE
+		if state == WALK and movement.x > 0: 
+			state = SLIDE
+
 	else:
 		var direction = sign(movement.x)
 		var velo = abs(movement.x)
@@ -59,9 +65,9 @@ func _physics_process(delta):
 			if movement.y == 0: state = IDLE 
 	# sprinting will just increase max speed
 	if Input.is_action_pressed("ui_sprint"):
-		movement.x = clamp(movement.x, -MAX_SPEED*1.5, MAX_SPEED*1.5)
+		movement.x = clamp(movement.x, -MAX_SPEED_SPRINT, MAX_SPEED_SPRINT)
 	else:
-		movement.x = clamp(movement.x, -MAX_SPEED, MAX_SPEED)
+		movement.x = clamp(movement.x, -MAX_SPEED_WALK, MAX_SPEED_WALK)
 
 	#jump will be higher if in duck state
 	if Input.is_action_pressed("ui_down") and state == IDLE:
@@ -77,20 +83,23 @@ func _physics_process(delta):
 		movement.y = -JUMPFORCE-400 if state == DUCK else -JUMPFORCE 
 		state = JUMP
 
+	if is_on_ceiling():
+		movement.y = 0
+
 	# Animations depending on which state mario is on
 	match state:
-		IDLE: $AnimationPlayer.play("idle_small")
-		WALK: 
-			if not get_node("AnimationPlayer").current_animation == "walk_small":
-    		get_node("AnimationPlayer").play("walk_small")
-		JUMP: $AnimationPlayer.play("jump_small")
-		FALL: $AnimationPlayer.play("jump_small")
+		IDLE: anim = "idle_small"
+		WALK: anim = "walk_small"
+		JUMP: anim = "jump_small"
+		FALL: anim = "jump_small"
 		SLIDE: 
-			$AnimationPlayer.play("slide_small")
+			anim = "slide_small"
 			$SFX_Slide.play()
-		DEAD: $AnimationPlayer.play("dead_small")
-		DUCK: $AnimationPlayer.play("duck_small")
+		DEAD: anim = "dead_small"
+		DUCK: anim = "duck_small"
 
+	if not $AnimationPlayer.current_animation == anim:
+		$AnimationPlayer.play(anim)
 	var collision = get_slide_count() - 1
 	if collision > -1:
 		var coll_body = get_slide_collision(collision)
@@ -99,6 +108,10 @@ func _physics_process(delta):
 				state = DEAD
 		if coll_body.collider.is_in_group("WinningFlag"):
 			print("you won")
+
+	if position.y > get_viewport().size.y:
+		state = DEAD
+
 	if state == DEAD:
 		emit_signal("dead")
 		
